89abf04ccfb9725a6817c0514fa8a6ab3c6eddda
Sean <sean.rich@mac.com>
2020-08-20 14:57:54 -0400

    Added demo of sidebar tracking problem

---
+++ SidebarToggleTest/MainWindowController.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+//        window?.toolbar?.insertItem(withItemIdentifier: .sidebarTrackingSeparator, at: 1)
+    }
+}
